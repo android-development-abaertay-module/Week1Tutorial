b57fa22541156e2f59508d248376c8ec462e452f
Bodovix <stbbap@gmail.com>
2019-01-28 15:28:59 +0000

    interfaces added

--- app/src/main/java/com/bodovix/week1tutorial/MainActivity.java
+++ app/src/main/java/com/bodovix/week1tutorial/MainActivity.java
@@ -2,13 +2,15 @@ package com.bodovix.week1tutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener {
 
     TextView textInput;
     String enteredNumber;
@@ -39,5 +41,40 @@ public class MainActivity extends AppCompatActivity {
         enteredNumber = enteredNumber + tag;
 
         textInput.setText(enteredNumber);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
 }
