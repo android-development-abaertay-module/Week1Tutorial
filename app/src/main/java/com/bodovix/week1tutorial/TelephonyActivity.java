9f9a5b1745368b7f2caab02677068c0d42b12234
Bodovix <stbbap@gmail.com>
2019-02-07 11:10:22 +0000

    Part one of Lab 4 Done

--- app/src/main/java/com/bodovix/week1tutorial/TelephonyActivity.java
+++ app/src/main/java/com/bodovix/week1tutorial/TelephonyActivity.java
@@ -1,13 +1,100 @@
 package com.bodovix.week1tutorial;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TelephonyActivity extends AppCompatActivity {
+
+    private static final int READ_PHONE_STATE_REQUEST = 0;
+    TelephonyManager telephonyManager;
+    TextView countryCodeTV;
+    TextView operatorTV;
+    TextView networkNameTV;
+    TextView deviceIdTV;
+    TextView osVersionTV;
+    TextView phoneNumberTV;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telephony);
+
+        checkPermissions();
+
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        countryCodeTV = findViewById(R.id.countryCodeTV);
+        operatorTV = findViewById(R.id.operatorTV);
+        networkNameTV = findViewById(R.id.networkNameTV);
+        deviceIdTV = findViewById(R.id.imeNumberTV);
+        osVersionTV = findViewById(R.id.osVersionTV);
+        phoneNumberTV = findViewById(R.id.phoneNumberTV);
+
+        btn1 = findViewById(R.id.btnOne);
+
+
+        showNetworkDetails();
+
     }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    READ_PHONE_STATE_REQUEST);
+
+        } else {
+            //permissions already granted
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_PHONE_STATE_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("gs", "granted");
+                //permissions granted
+            } else {
+                Intent intent = new Intent(TelephonyActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void showNetworkDetails() {
+        countryCodeTV.setText(telephonyManager.getNetworkCountryIso());
+        operatorTV.setText(telephonyManager.getNetworkOperator());
+        networkNameTV.setText(telephonyManager.getNetworkOperatorName());
+        try {
+            deviceIdTV.setText(telephonyManager.getImei());
+        } catch (SecurityException e) {
+            deviceIdTV.setText("Permission Required");
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        phoneNumberTV.setText(String.format("%s Phone number", telephonyManager.getLine1Number()));
+    }
+
 }
