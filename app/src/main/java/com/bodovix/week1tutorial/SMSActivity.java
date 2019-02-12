60dc4dbeb0631ce9212553148bb44e2ecdeafb88
Bodovix <stbbap@gmail.com>
2019-02-12 10:54:06 +0000

    works with inline classes

--- app/src/main/java/com/bodovix/week1tutorial/SMSActivity.java
+++ app/src/main/java/com/bodovix/week1tutorial/SMSActivity.java
@@ -1,26 +1,66 @@
 package com.bodovix.week1tutorial;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SMSActivity extends AppCompatActivity {
 
     private static final int SEND_SMS_REQUEST = 0;
 
+    EditText smsText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
 
+        smsText = findViewById(R.id.SMSTxt);
         checkPermissions();
+
+        //Dynamically registered Receiver on create
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d("gwyd","on Receive method hit");
+                switch(getResultCode()){
+                    case Activity.RESULT_OK:
+                        //display SENT message
+                        Toast.makeText(context,"SMS SENT: receiver hit",Toast.LENGTH_SHORT).show();
+                        Log.d("gwyd","SMS SENT");
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        // display ERROR message
+                        Toast.makeText(context,"SMS ERROR: receiver hit",Toast.LENGTH_SHORT).show();
+                        Log.d("gwyd","SMS ERROR");
+
+                        break;
+                }
+            }
+        }, new IntentFilter("SENT_SMS_ACTION") );
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+            // show delivery confirmation message
+                Toast.makeText(context,"SMS Delivered: receiver hit",Toast.LENGTH_SHORT).show();
+                Log.d("gwyd","SMS DELIVERED");
+            }
+        }, new IntentFilter("DELIVERED_SMS_ACTION"));
+
     }
 
     private void checkPermissions() {
@@ -34,6 +74,8 @@ public class SMSActivity extends AppCompatActivity {
 
         } else {
             //permissions already granted
+            Log.d("gwyd","send sms permistion already granted");
+
         }
     }
 
@@ -44,7 +86,7 @@ public class SMSActivity extends AppCompatActivity {
             case SEND_SMS_REQUEST:
                 Log.d("gwyd", "onRequestPermissionsResult: ");
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("gs", "granted");
+                    Log.d("gwyd", "granted");
                     //permissions granted
                     Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
                 } else {
@@ -53,5 +95,23 @@ public class SMSActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    public void sendBtn_Clicked(View view) {
+        //any request code
+        int requestCode = 1;
+        int requestCodeTwo = 2;
+        String message = smsText.getText().toString();
+        String number = "5556";
+
+        Intent sentIntent = new Intent("SENT_SMS_ACTION");
+        Intent deliverdIntent = new Intent("DELIVERED_SMS_ACTION");
+        PendingIntent sentPI = PendingIntent.getBroadcast(getApplicationContext(), requestCode, sentIntent, 0);
+        PendingIntent deliveryPI = PendingIntent.getBroadcast(getApplicationContext(),requestCodeTwo,deliverdIntent,0);
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, message, sentPI, deliveryPI);
+        Log.d("gwyd","BTN CLICKED");
+
     }
 }
